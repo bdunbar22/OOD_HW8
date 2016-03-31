1d0a27088fa630a0413dd8461e9f81d7161f8927
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-03-31 13:42:48 -0400

    Start the controller interface

---
+++ src/cs3500/music/controller/IMusicController.java
@@ -0,0 +1,12 @@
+package cs3500.music.controller;
+
+/**
+ * This should allow for modifications to be made to the model because the controller will have
+ * access to call the model functions.
+ *
+ *
+ * Created by Ben on 3/30/16.
+ */
+public interface IMusicController {
+
+}
