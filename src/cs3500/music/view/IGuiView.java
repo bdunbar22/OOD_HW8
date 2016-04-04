1af3a532616ab59a81b49bf0128e05fb1a9ba48d
sletcher <letcher.s@husky.neu.edu>
2016-04-03 23:51:50 -0400

    Added IGuiView and redirected GuiViewFrame to implement it instead of IMusicView

--- src/cs3500/music/view/IGuiView.java
+++ src/cs3500/music/view/IGuiView.java
@@ -1,7 +1,5 @@
 package cs3500.music.view;
 
-import java.awt.*;
-
 /**
  * Interface for use explicitly by the GuiView
  */
@@ -10,11 +8,4 @@ public interface IGuiView extends IMusicView {
    * Sets the view frame for the Gui to visible
    */
   void initialize();
-
-  /**
-   * Sets the preferred size for the Gui view frame
-   *
-   * @return A Dimension containing the preferred gui size
-   */
-  Dimension getPreferredSize();
 }
