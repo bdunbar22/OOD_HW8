2706a8b999ad1fe4873123c39c608029140dc6a7
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-04-05 14:05:36 -0400

    Composite view should have listeners now because it now implements IGuiView and allows the listeners to be added to the private gui view field.

--- src/cs3500/music/view/CompositeView.java
+++ src/cs3500/music/view/CompositeView.java
@@ -1,13 +1,18 @@
 package cs3500.music.view;
 
 
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
 /**
- * A view that implements the IMusicView and allows for the composite display of the Midi and
+ * A view that implements the IGuiView and allows for the composite display of the Midi and
  * Gui views at the same time.
+ * This view will implement the IGuiView so that it offers the add listeners for the keyboard
+ * and mouse, which will allow editing to be possible.
  *
  * Created by Ben on 4/4/16.
  */
-public class CompositeView implements IMusicView {
+public class CompositeView implements IGuiView {
     IMusicView midiViewImpl;
     IGuiView guiViewFrame;
 
@@ -34,5 +39,31 @@ public class CompositeView implements IMusicView {
     public void update(IViewPiece viewPiece) {
         guiViewFrame.update(viewPiece);
         midiViewImpl.update(viewPiece);
+    }
+
+    /**
+     * Want the gui part of this view to listen to the keyboard.
+     * @param listener for keys
+     */
+    @Override
+    public void addKeyListener(KeyListener listener) {
+        guiViewFrame.addKeyListener(listener);
+    }
+
+    /**
+     * Want the gui part of this view to listen to the mouse.
+     * @param listener for mouse
+     */
+    @Override
+    public void addMouseListener(MouseListener listener) {
+        guiViewFrame.addMouseListener(listener);
+    }
+
+    /**
+     * Offer the gui frames initialize.
+     */
+    @Override
+    public void initialize(){
+        guiViewFrame.initialize();
     }
 }
