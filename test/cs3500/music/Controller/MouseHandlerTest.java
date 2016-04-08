00fb2eb94e92d53ec4bdf6fbb1cfaf0f4a04ec97
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-04-07 20:55:19 -0400

    Set up testing for mouse handler.

--- test/cs3500/music/Controller/MouseHandlerTest.java
+++ test/cs3500/music/Controller/MouseHandlerTest.java
@@ -14,38 +14,64 @@ import org.junit.Test;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 
 import static org.junit.Assert.*;
 
 /**
+ * Test that the mouse handler works correctly.
+ *
  * Created by Ben on 4/7/16.
  */
 public class MouseHandlerTest {
     private Controller.Toggle toggle;
+    private String testString;
 
-    @Test public void testMouseClicked() throws Exception {
+    /**
+     * Ensure mouse click events are called correctly.
+     */
+    @Test
+    public void testMouseClicked() {
+        testString = "";
+        MouseListener mockListener = createTestHandler();
+        Component jpanel = new JPanel();
+        MouseEvent mouseEvent = new MouseEvent(jpanel, // which
+            MouseEvent.MOUSE_CLICKED, // type of mouse event
+            System.currentTimeMillis(), // when
+            0, // no modifiers
+            10, 10, // where: at (10, 10}
+            1, // only 1 click
+            false); // not a popup trigger
+
+        jpanel.addMouseListener(mockListener);
+        jpanel.dispatchEvent(mouseEvent);
+        mockListener.mouseClicked(mockKey);
+
+        assertEquals("r end zero home one m c a l space b up down right left t ", testHolder);
+    }
+
+    @Test
+    public void testMousePressed() {
 
     }
 
-    @Test public void testMousePressed() throws Exception {
+    @Test
+    public void testMouseReleased() {
 
     }
 
-    @Test public void testMouseReleased() throws Exception {
+    @Test
+    public void testMouseEntered() {
 
     }
 
-    @Test public void testMouseEntered() throws Exception {
-
-    }
-
-    @Test public void testMouseExited() throws Exception {
+    @Test
+    public void testMouseExited() {
 
     }
 
     /**
-     * Create fake maps and put in a handler
+     * Create a handler to test
      */
     private MouseListener createTestHandler() {
         MouseListener mouseHandler = new MouseHandler(new mouseHelper());
@@ -54,133 +80,84 @@ public class MouseHandlerTest {
 
 
     /**
-     * Provide the mouse handler with necessary functions from the controller.
+     * Create a mouse helper.
      */
     class mouseHelper implements MouseHandlerHelper {
         //provide use of controller's deleteNote
-        @Override public void deleteNoteFromMouse(int x, int y) {
+        @Override
+        public void deleteNoteFromMouse(int x, int y) {
             deleteNote(x, y);
         }
 
         //provide use of controller's checkForNote
-        @Override public boolean checkForNoteFromMouse(int x, int y) {
+        @Override
+        public boolean checkForNoteFromMouse(int x, int y) {
             return checkForNote(x, y);
         }
 
         //provide use of controller's addNote(x,y,length value)
-        @Override public void addNoteFromMouse(int x, int y, int dx) {
+        @Override
+        public void addNoteFromMouse(int x, int y, int dx) {
             addNote(x, y, dx);
         }
 
         //provide use of controller's add note with all fields.
-        @Override public void addNoteFromMouse(int x, int y, int length, int instrument,
+        @Override
+        public void addNoteFromMouse(int x, int y, int length, int instrument,
             int volume) {
             addNote(x, y, length, instrument, volume);
         }
 
         //provide use of controller's  move note
-        @Override public void moveNoteFromMouse(INote old, Point point) {
+        @Override
+        public void moveNoteFromMouse(INote old, Point point) {
             moveNote(old, point);
         }
 
         //provide use of controller's get note
-        @Override public INote getNoteFromMouse(int x, int y) {
+        @Override
+        public INote getNoteFromMouse(int x, int y) {
             return getNote(x, y);
         }
 
         ////provide use of controller's toggle status.
-        @Override public Controller.Toggle getMoveToggleFromMouse() {
+        @Override
+        public Controller.Toggle getMoveToggleFromMouse() {
             return toggle;
         }
 
         //provide use of controllers note creation with user location input
-        @Override public void addNoteLocationNeeded(int dx) {
+        @Override
+        public void addNoteLocationNeeded(int dx) {
             addNotePromptLocation(dx);
         }
     }
 
-    /**
-     * Allow for a note to be added to the piece of music in
-     * question. The note pitch, octave and start beat will be determined based on the location
-     * of a mouse click. The volume will be based off of the drag length of the mouse.
-     */
     private void addNote(int x, int y, int length) {
-        //TODO: something
+        testString += "addNote1";
     }
 
-    /**
-     * Allow for the location of the note to be given by the user as input
-     * length of note is based on mouse drag distance.
-     * <p>
-     * Invalid data will be ignored and the app will continue to run.
-     */
     private void addNotePromptLocation(int length) {
-        //TODO: something
+        testString += "addNote2";
     }
 
-    /**
-     * Add a new note by passing params
-     *
-     * @param x          location
-     * @param y          location
-     * @param length     duration of the note's sustain
-     * @param instrument instrument
-     * @param volume     volume
-     */
     private void addNote(int x, int y, int length, int instrument, int volume) {
-        //TODO: something
+        testString += "addNote3";
     }
 
-    /**
-     * Move a note to a different starting beat, pitch and octave.
-     *
-     * @param note   that is being moved
-     * @param newPos to move to on view
-     */
     private void moveNote(INote note, Point newPos) {
-        //TODO: something
+        testString += "moveNote";
     }
 
-    /**
-     * If possible will delete the chosen note from the piece.
-     *
-     * @param x coordinate in view.
-     * @param y coordinate in view.
-     */
     private void deleteNote(final int x, final int y) {
-        //TODO: something
+        testString += "deleteNote1";
     }
 
-    /**
-     * Delete the given note
-     *
-     * @param note to delete
-     */
-    private void deleteNote(INote note) {
-        //TODO: something
-    }
-
-    /**
-     * Determine if getting a note will work.
-     *
-     * @param x location
-     * @param y location
-     * @return boolean if note will be retrieved correctly.
-     */
     private boolean checkForNote(final int x, final int y) {
-        //TODO: something
         return true;
     }
 
-    /**
-     * Retrieves the note that was clicked on in the GUI.
-     *
-     * @param x mouse location
-     * @param y mouse location
-     * @return note that was found
-     */
     private INote getNote(final int x, final int y) {
-        //TODO: something
         return new Note(Pitch.A, new Octave(4), 4, 3);
     }
 }
